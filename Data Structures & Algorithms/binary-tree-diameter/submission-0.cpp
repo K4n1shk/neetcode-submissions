66c7bb5cf73121e675bdcbf0bf1f97f3f77826d1
Kanishk Kunal <60432823+K4n1shk@users.noreply.github.com>
2026-05-02 01:36:21 +0530

    Update: binary-tree-diameter - submission-0

--- Data Structures & Algorithms/binary-tree-diameter/submission-0.cpp
+++ Data Structures & Algorithms/binary-tree-diameter/submission-0.cpp
@@ -12,10 +12,19 @@
 
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if (root == nullptr){
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res = 0;
+        dfs(root,res);
+        return res;
+    }
+
+    int dfs(TreeNode* root, int &res) {
+        if(!root){
             return 0;
         }
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+        int left = dfs(root->left, res);
+        int right = dfs(root->right, res);
+        res = max(res,left+right);
+        return 1 + max(left, right);
     }
 };
