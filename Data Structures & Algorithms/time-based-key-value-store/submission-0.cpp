3b315b413eb8d4838eab11724bcc853bcd734d39
Kanishk Kunal <60432823+K4n1shk@users.noreply.github.com>
2026-05-24 21:57:07 +0530

    Update: time-based-key-value-store - submission-0

--- Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -1,28 +1,29 @@
-class Solution {
+class TimeMap {
+private:
+    unordered_map<string, vector<pair<int, string>>> keyStore;
+
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+    TimeMap() {}
 
-        while (l <= r) {
-            int mid = (l + r) / 2;
-            if (target == nums[mid]) {
-                return mid;
-            }
+    void set(string key, string value, int timestamp) {
+        keyStore[key].emplace_back(timestamp, value);
+    }
 
-            if (nums[l] <= nums[mid]) {
-                if (target > nums[mid] || target < nums[l]) {
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
-                }
+    string get(string key, int timestamp) {
+        auto& values = keyStore[key];
+        int left = 0, right = values.size() - 1;
+        string result = "";
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (values[mid].first <= timestamp) {
+                result = values[mid].second;
+                left = mid + 1;
             } else {
-                if (target < nums[mid] || target > nums[r]) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+                right = mid - 1;
             }
         }
-        return -1;
+
+        return result;
     }
 };
