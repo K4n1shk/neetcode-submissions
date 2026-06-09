a7bd0e01b66891a9416d942f050aaed2b163d820
Kanishk Kunal <60432823+K4n1shk@users.noreply.github.com>
2026-06-09 22:54:14 +0530

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.cpp
@@ -0,0 +1,62 @@
+class DSU{
+    private:
+        vector<int> parent;
+        vector<int> rank;
+        int component;
+    
+    public:
+        DSU(int n){
+            component = n;
+            parent.resize(n);
+            rank.resize(n,1); // size + initialization
+            for(int i = 0;i<n;i++){
+                parent[i] = i;
+            }
+        }
+
+        int find(int x){
+            if(parent[x] == x){
+                return x;
+            }
+            parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        bool unite(int a, int b){
+            int rootA = find(a);
+            int rootB = find(b);
+
+            if (rootA == rootB) return false;
+
+            if(rank[rootA] < rank[rootB]){
+                parent[rootA] = rootB;
+            }else if (rank[rootA] > rank[rootB]){
+                parent[rootB] = rootA;
+            }else{
+                parent[rootA] = rootB;
+                rank[rootB]++;
+            }
+            component--;
+            return true;
+        }
+
+        int components(){
+            return component;
+        }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if(edges.size() > n-1){
+            return false;
+        }
+        DSU dsu(n);
+        for(auto &edge: edges){
+            if(!dsu.unite(edge[0],edge[1])){
+                return false;
+            }
+        }
+        return dsu.components() == 1;
+    }
+};
